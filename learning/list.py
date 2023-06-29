8e7f4f7186744288d7e05d75120341232811eb41
devfaoed <faithadedokun2018@gmail.com>
2023-06-29 23:45:32 +0100

    tasks

--- learning/list.py
+++ learning/list.py
@@ -29,3 +29,15 @@ print(list4)
 # iterating list using for loop
 for x in list4:
     print("value:", x)
+
+
+# more example
+
+employee_list = [{"id": 12345, "name": "John", "department": "Kitchen"}, {"id": 12458, "name": "Paul", "department": "House Floor"}]
+
+def get_employee(id): 
+    for employee in employee_list:
+        if employee['id'] == id:
+            return employee
+
+print(get_employee(12458));
