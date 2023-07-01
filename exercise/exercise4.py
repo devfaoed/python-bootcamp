62e2dd64ced0b26c974d00bfaa6e0f78c02a5dfe
devfaoed <faithadedokun2018@gmail.com>
2023-07-01 18:00:47 +0100

    solving problem

---
+++ exercise/exercise4.py
@@ -0,0 +1,32 @@
+employee_list = [
+   {"id": 12345, "name": "John", "department": "Kitchen"},
+   {"id": 12456, "name": "Paul", "department": "House Floor"},
+   {"id": 12478, "name": "Sarah", "department": "Management"},
+   {"id": 12434, "name": "Lisa", "department": "Cold Storage"},
+   {"id": 12483, "name": "Ryan", "department": "Inventory Mgmt"},
+   {"id": 12419, "name": "Gill", "department": "Cashier"}
+]
+
+def mod(employee_list):
+   temp = employee_list['name'] + "_" + employee_list["department"]
+   return temp
+
+def to_mod_list(employee_list):
+    return list(map(mod, employee_list))
+
+def generate_usernames(mod_list):
+    return [item.replace(" ", "_") for item in mod_list]
+
+def map_id_to_initial(employee_list):
+    return {employee["name"][0]: employee["id"] for employee in employee_list}
+
+def main():
+   mod_emp_list = to_mod_list(employee_list)
+   print("Modified employee list: " + str(mod_emp_list) + "\n")
+
+   print(f"List of usernames: {generate_usernames(mod_emp_list)}\n")
+
+   print(f"Initials and ids: {map_id_to_initial(employee_list)}")
+
+if __name__ == "__main__":
+   main()
