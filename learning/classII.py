2cf330c1bdb0a2219f15633788aadeb9ff89a257
devfaoed <faithadedokun2018@gmail.com>
2023-07-13 02:09:41 +0100

    adding variables, method and attributes on class

--- learning/classII.py
+++ learning/classII.py
@@ -18,3 +18,19 @@ print(rice.items)
 
 print(pizza.contents())
 print(rice.contents())
+
+
+# another example
+# Define class MyFirstClass
+class MyFirstClass:
+    # Define string variable called index
+    index  = "Author-Book"
+    # Define function hand_list()
+    def hand_list(self, philosopher, book):
+        # variable + “ wrote the book: ” + variable
+        print( philosopher + " wrote the book: " + book)
+        pass
+        
+# Call function handlist()
+whodunnit = MyFirstClass()
+print(whodunnit.hand_list("Sun Tzu", "The Art of War",))
