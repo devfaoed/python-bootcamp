ca0cfe93ecd7adcfba9e01317b9c4e71cb085889
devfaoed <faithadedokun2018@gmail.com>
2023-07-13 01:13:04 +0100

    learning class, instance & attribute

--- learning/class.py
+++ learning/class.py
@@ -5,3 +5,29 @@ class myClass:
 
 myc = myClass()
 print(myc.greet())
+
+
+
+class House:
+    '''
+    This is a stub for a class representing a house that can be used to create objects and evaluate different metrics that we may require in constructing it.
+    '''
+    num_rooms = 5
+    bathrooms = 2
+    def cost_evaluation(self):
+        print(self.num_rooms)
+        pass
+        # Functionality to calculate the costs from the area of the house
+
+house = House()
+print(house.num_rooms)
+print(House.num_rooms)
+
+house.num_rooms = 7
+print(house.num_rooms)
+print(House.num_rooms)
+
+# adding attribute dirrectly to the class instance
+House.num_rooms = 7
+print(house.num_rooms)
+print(House.num_rooms)
