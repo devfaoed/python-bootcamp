b6d9ddd336d3d1a98c8c0f9294609ca2300f4f8a
devfaoed <faithadedokun2018@gmail.com>
2023-07-14 06:28:35 +0100

    learning how to use math module using the import statement in python

---
+++ learning/learning import/math.py
@@ -0,0 +1,24 @@
+import math
+root = math.sqrt(9)
+print(root)
+print(int(root))
+
+# better way of optimizing the above code
+from math import sqrt
+squareRoot = sqrt(9)
+print("the sqaure root of 9 is", squareRoot)
+
+# getting consine value of numbers using math function
+import math
+cosine = math.cos(0)
+print(cosine)
+
+# gettting the factorila of any number
+# from math import factorial, log10, sqrt
+from math import *
+fac10 = factorial(10)
+print("The factorial of 10 is", fac10)
+
+# getting the log10 of x number
+x = log10(20)
+print("log10 of 20 is", x)
